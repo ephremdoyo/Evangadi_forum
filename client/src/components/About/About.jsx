d39469f7cffe64ee871a6510c6d90b202c56960a
ephremdoyo <ephremdoyo@gmail.com>
2024-10-11 09:53:02 +0300

    about component implemented

---
+++ client/src/components/About/About.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import classes from "./about.module.css";
+import { Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <section className={classes.outer_about_wrapper}>
+      <section className={classes.about_container}>
+        <div className={classes.about}>
+          <p>About</p>
+        </div>
+        {/* title container  */}
+        <div>
+          <h1>Evangadi Networks</h1>
+        </div>
+        {/* paragraph container  */}
+        <div className={classes.paragraph_container}>
+          <p>
+            No matter what stage of life you are in, whether you're just
+            starting elementary school or being promoted to CEO of a Fortune 500
+            company, you have much to offer to those who are trying to follow in
+            your footsteps.
+          </p>
+          <p>
+            Wheather you are willing to share your knowledge or you are just
+            looking to meet mentors of your own, please start by joining the
+            network here.
+          </p>
+        </div>
+        <div>
+          <Link to="/how-it-works">
+            <button className={classes.about_btn}>HOW IT WORKS</button>
+          </Link>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default About;
