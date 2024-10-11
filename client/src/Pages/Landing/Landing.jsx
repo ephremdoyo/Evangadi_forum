15dd987a787108ff1598bd20b973d7f2516eaeac
ephremdoyo <ephremdoyo@gmail.com>
2024-10-11 10:04:22 +0300

    Implement landing page layout and functionality

---
+++ client/src/Pages/Landing/Landing.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import About from "../../conponents/About/About";
+import Login from "../../conponents/Login/Login";
+import SignUp from "../../conponents/SignUp/SignUp";
+import classes from "./landing.module.css";
+
+const Landing = () => {
+  const [activeComponent, setActiveComponent] = useState("Login");
+  const showToLogin = () => setActiveComponent("Login");
+  const showToSignUp = () => setActiveComponent("SignUp");
+  return (
+    <section className={classes.outer_landing_page}>
+      <section className={classes.landing_page_container}>
+        <div>
+          <section className={classes.outer_signUp_container}>
+            <section className={classes.signUp_container}>
+              {activeComponent === "Login" ? (
+                <Login switchToSignUp={showToSignUp} />
+              ) : (
+                <SignUp switchToLogin={showToLogin} />
+              )}
+            </section>
+          </section>
+        </div>
+        <div>
+          <About />
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Landing;
